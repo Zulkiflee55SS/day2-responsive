a2e9b00cfac851c01226e9f4ddf5fd3405ce175c
Zulkiflee <s67319010002@ptnicec.ac.th>
2025-03-13 22:56:31 +0700

    22

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
 document.querySelector(".menu-toggle").addEventListener("click", function() {
     document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+    link.addEventListener("click", function() {
+        document.querySelector(".nav-menu").classList.remove("show");
+    });
 });
